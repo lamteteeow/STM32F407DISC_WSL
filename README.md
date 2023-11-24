6121f29cb1051943a3c599627b1ae7ce7319502b
lamteteeow <lamteteeow@gmail.com>
2023-11-24 03:07:12 +0100

    modified package layout

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Microcontroller: STM32F407VGT - (deprecated name STM32F4DISCOVERY)
 Peripherals:
 
 - Memory IC for UID using EEPROM - part SN: 25AA02UID-I/SN
+- MicroSD from DFROBOT - part SN: DFR0229
+- Infra red thermometer from Olimex - part SN: MOD-IR-TEMP
 - to be added
--- test-size/src/lib.rs
+++ test-size/src/lib.rs
@@ -1,11 +1,21 @@
 #![no_std]
 // #![allow(non_camel_case_types)]
 
+pub use stm32f407g_disc as board;
 pub use stm32f4xx_hal as hal;
 
-pub use crate::hal::pac::interrupt::*;
-pub use crate::hal::pac::Interrupt;
-pub use crate::hal::pac::Peripherals;
+pub use cortex_m_rt::entry;
+pub use hal::pac::interrupt::*;
+pub use hal::pac::Interrupt;
+pub use hal::pac::Peripherals;
+
+pub use board::{
+    hal::delay::Delay,
+    hal::prelude::*,
+    hal::stm32,
+    led::{LedColor, Leds},
+};
 
 // pub mod accelerometer;
 // pub mod led;
+// pub mod dummy;
