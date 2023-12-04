f17901c18e69cbac6300caeda2673434efb12b35
lamteteeow <lamteteeow@gmail.com>
2023-12-04 06:04:34 +0100

    start using probe-rs for debugging

--- ir_sensor/src/main.rs
+++ ir_sensor/src/main.rs
@@ -2,15 +2,14 @@
 #![no_std]
 #![no_main]
 
-// use board::gpio::gpiob::PB6;
 use board::hal::{delay::Delay, i2c::*, prelude::*, stm32}; // PAC = peripheral access crate, here equal stm32
-use cortex_m::Peripherals;
+use cortex_m::{asm, Peripherals};
 use cortex_m_rt::entry;
 use driver::{Mlx9061x, SlaveAddr};
+use mlx9061x as driver;
+// use panic_probe as _;
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
-// use panic_halt as _;
-use mlx9061x as driver;
 use stm32f407g_disc as board;
 
 #[entry]
@@ -29,7 +28,6 @@ fn main() -> ! {
 
     let scl = gpiob.pb6.into_alternate_af4_open_drain();
     let sda = gpiob.pb7.into_alternate_af4_open_drain();
-    // let pins: PINS<I2C> = (scl, sda);
 
     // Setup I2C1 using PB6/PB7 at 400kHz bitrate (fast mode)
     let i2c = I2c::new(dp.I2C1, (scl, sda), 400.khz(), clocks);
@@ -44,15 +42,16 @@ fn main() -> ! {
     let mut sensor = maybe_sensor.unwrap();
 
     loop {
-        if let Ok(obj_temp) = sensor.object1_temperature() {
-            rprintln!("Object temperature: {:.2}ºC", obj_temp);
+        // asm::bkpt();
+        if let Ok(obj_temp) = sensor.object1_temperature_as_int() {
+            rprintln!("Object temperature: {:.2}oC", obj_temp);
         } else {
-            rprintln!("Read error.");
+            rprintln!("Read object error.");
         }
-        if let Ok(ambient_temp) = sensor.ambient_temperature() {
+        if let Ok(ambient_temp) = sensor.ambient_temperature_as_int() {
             rprintln!("Ambient temperature: {:.2}ºC\n", ambient_temp);
         } else {
-            rprintln!("Read error.");
+            rprintln!("Read ambient error.");
         }
 
         delay.delay_ms(1000_u16);
