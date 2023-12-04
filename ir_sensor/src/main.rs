a60a487e435686e0be1854fb62b48d50feb350c9
lamteteeow <lamteteeow@gmail.com>
2023-12-04 04:11:44 +0100

    reading temperatures

--- ir_sensor/src/main.rs
+++ ir_sensor/src/main.rs
@@ -4,19 +4,20 @@
 
 // use board::gpio::gpiob::PB6;
 use board::hal::{delay::Delay, i2c::*, prelude::*, stm32}; // PAC = peripheral access crate, here equal stm32
-use cortex_m_rt::entry;
-// use rtt_target::{rprintln, rtt_init_print};
-// use cortex_m_semihosting::hprintln;
 use cortex_m::Peripherals;
+use cortex_m_rt::entry;
+use driver::{Mlx9061x, SlaveAddr};
 use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+// use panic_halt as _;
+use mlx9061x as driver;
 use stm32f407g_disc as board;
-
-// impl<I2C1> PinSda<I2C1> for Pins<I2C1> {}
 
 #[entry]
 fn main() -> ! {
-    // rtt_init_print!();
-    let mut dp = stm32::Peripherals::take().unwrap();
+    rtt_init_print!();
+
+    let dp = stm32::Peripherals::take().unwrap();
     let cp = Peripherals::take().unwrap();
 
     let gpiob = dp.GPIOB.split();
@@ -32,10 +33,28 @@ fn main() -> ! {
 
     // Setup I2C1 using PB6/PB7 at 400kHz bitrate (fast mode)
     let i2c = I2c::new(dp.I2C1, (scl, sda), 400.khz(), clocks);
+    rprintln!("I2C initialized");
 
-    // rprintln!("I2C initialized");
+    let addr = SlaveAddr::default();
+    let maybe_sensor = Mlx9061x::new_mlx90614(i2c, addr, 5);
+    if let Err(err) = maybe_sensor {
+        rprintln!("I2C error: {:?}", err);
+        loop {}
+    }
+    let mut sensor = maybe_sensor.unwrap();
 
     loop {
-        continue;
+        if let Ok(obj_temp) = sensor.object1_temperature() {
+            rprintln!("Object temperature: {:.2}ºC", obj_temp);
+        } else {
+            rprintln!("Read error.");
+        }
+        if let Ok(ambient_temp) = sensor.ambient_temperature() {
+            rprintln!("Ambient temperature: {:.2}ºC\n", ambient_temp);
+        } else {
+            rprintln!("Read error.");
+        }
+
+        delay.delay_ms(1000_u16);
     }
 }
